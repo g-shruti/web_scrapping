64b78427111d6154cb8a4d600804eab1f9da0402
Shruti Jain <jainshruti.rattan@gmail.com>
2021-08-25 20:08:54 +0530

    comment fetched

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 const request = require("request");
 const cheerio = require("cheerio");
 
-request("",callback);
+request("https://www.espncricinfo.com/series/ipl-2021-1249214/punjab-kings-vs-delhi-capitals-29th-match-1254086/ball-by-ball-commentary",callback);
+
+// word wrap -- Alt+Z
 
 function callback(error,response,html)
 {
     if(!error)
     {
         const manipulationTool = cheerio.load(html);
-        let comment = manipulationTool().text;
-        console.log(comment);
+        let comments = manipulationTool(".col-14.col-md-15.col-lg-14 .match-comment-long-text p");
+        let reqcomment = manipulationTool(comments[0]).text();
+
+        console.log(reqcomment);
     }
 }
