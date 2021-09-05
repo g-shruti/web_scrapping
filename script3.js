c712a473849467070d3071870159bd7b4866599d
Shruti Jain <jainshruti.rattan@gmail.com>
2021-09-05 20:53:13 +0530

    up

--- script3.js
+++ script3.js
@@ -1,5 +1,7 @@
 const request = require("request");
 const cheerio = require("cheerio");
+let fs = require("fs");
+let data ={};
 
 request("https://www.espncricinfo.com/series/ipl-2021-1249214/punjab-kings-vs-delhi-capitals-29th-match-1254086/full-scorecard",callback);
 
@@ -23,7 +25,7 @@ function profileProcessor(name,url){
     request(url, function(error,response,html){
             const manipulationTool = cheerio.load(html);
             let list =manipulationTool(".player-card-description.gray-900");
-            console.log(name);
-            console.log (manipulationTool(list[1]).text());
+            data[name] = manipulationTool(list[1]).text();
+            fs.writeFileSync("data.json",JSON.stringify(data));
         });
 }
