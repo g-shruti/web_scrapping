23b0e1d6e123f6829fdef4e9f6d3cc546634bc7c
Shruti Jain <jainshruti.rattan@gmail.com>
2021-09-06 20:17:58 +0530

    up

--- GithubScrap/github.js
+++ GithubScrap/github.js
@@ -1,5 +1,7 @@
 const request = require("request");
 const cheerio = require("cheerio");
+let fs = request("fs");
+let data = {};
 
 request("https://github.com/topics",callback);
 
@@ -13,8 +15,23 @@ function callback(error,response,html)
         
         for(let i=0;i<allAnchors.length;i++)
         {
-            console.log("https://github.com" + manipulationTool(allAnchors[i]).attr("href"));
-            console.log(manipulationTool(manipulationTool(allAnchors[i]).find("p")[0]).text().trim());
+            topicProcessor("https://github.com" + manipulationTool(allAnchors[i]).attr("href"),manipulationTool(manipulationTool(allAnchors[i]).find("p")[0]).text().trim());
+        
         }
     }
+}
+
+function topicProcessor(url,name)
+{
+    request(url,function(error,response,html){
+        const mt = cheerio.load(html);
+        let allHeadings = mt(".f3.color-text-secondary.text-normal.lh-condensed");
+        allHeadings = allHeadings.slice(0 , 5);
+
+        for(let i = 0; i<allHeadings.length ; i++){
+            console.log(mt(mt(allHeadings[i]).find("a")[1]).text().trim());
+            console.log("https://github.com/" + mt(mt(allHeadings[i]).find("a")[1]).attr("href"));
+            console.log("__________");
+        }
+    });
 }
